3bdee3871413f3ee2092ed66f06a43999e057a6b
Danilo Henrique Cordeiro <dancorde@gmail.com>
2019-06-20 10:15:34 -0300

    busca OK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
   end
 
   def search
-    @institutions = Institution.all
+    @institutions = Institution
+      .where('state LIKE ? AND city LIKE ? AND necessidade LIKE ?', params[:state], params[:city][:city_id], params[:need])
+      .joins(:needs)
   end
 end
